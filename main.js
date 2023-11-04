d36d36476f5638e95b9c1b83b6dc802eb7c943f6
Ruslan <greenexe@icloud.com>
2023-11-04 18:38:29 +0600

    start

---
+++ main.js
@@ -0,0 +1,41 @@
+
+// const navigationList = document.querySelector('.navigation_container');
+// const navItem = document.querySelector('.nav_item');
+// const navItemLink = document.getElementsByClassName('nav-item');
+// const listButton = document.querySelector('.list_button');
+// const sizeScreen = window.document.documentElement.clientWidth;
+// const lissenScreen = window.lissenScreen.clientWidth;
+
+// // Функция которая вызывает функцию адаптива
+// const controlScreen = (screen) => {
+//   if (600 >= screen) {
+//     // вызываем адаптив для смартфонов
+//     phoneScreenControl(screen);
+//   }if (600 < screen && screen <= 1000){
+//     // вызываем адаптив для планшетов
+//     tabletScreenControl();
+//   }if (screen > 1000){
+//     // вызываем адаптив для мониторов
+//     maxScreenControl();
+//   }
+// }
+// // Адаптив для смартфонов
+//   const phoneScreenControl = (screen) => {
+//     window.addEventListener('resize'){
+      
+//     }
+//   }
+// // Адаптив для планшета
+//   const tabletScreenControl = (sizeScreen) => {
+    
+//     console.log(' tablet Screen')
+//   };
+// // Адаптив для больших экранов
+//   const maxScreenControl = () => {
+//     console.log('max Screen')
+//   };
+
+
+
+// // Вызовы функции
+// controlScreen(sizeScreen)
